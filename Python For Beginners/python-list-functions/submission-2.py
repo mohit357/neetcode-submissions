f6b47850a22c497090543ac849c0ea7741ec6612
mohit357 <51874018+mohit357@users.noreply.github.com>
2026-09-09 14:44:20 +0530

    Add: python-list-functions - submission-2

---
+++ Python For Beginners/python-list-functions/submission-2.py
@@ -0,0 +1,38 @@
+from typing import List # this is used to add type hints for List type
+
+def get_sum(nums: List[int]) -> int:
+    sumosa = 0
+    for i in nums:
+        sumosa += i
+    return sumosa
+
+def get_min(nums: List[int]) -> int:
+    minmosa = nums[0]
+    counter = 0
+    for num in nums:
+        if num < minmosa:
+            minmosa = num
+        else:
+            pass
+    return minmosa
+
+def get_max(nums: List[int]) -> int:
+    maxmosa = nums[0]
+    
+    for i in nums:
+        if i > maxmosa:
+            maxmosa = i
+        else :
+            pass
+    return maxmosa
+    
+
+# do not modify below this line
+print(get_sum([1, 2, 3, 4, 5]))
+print(get_sum([5, 4, 5, 6]))
+
+print(get_min([7, 3, 4, 5]))
+print(get_min([5, 4, 5, 6]))
+
+print(get_max([7, 3, 4, 5]))
+print(get_max([5, 4, 5, 6]))
